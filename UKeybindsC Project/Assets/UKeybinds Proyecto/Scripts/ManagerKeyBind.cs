d766a323b810f5b1d52109e1b32d9d6928151ef9
Moon Antonio <pinchodev91@gmail.com>
2017-07-28 01:35:44 +0200

    KeyBind desarrollado

--- UKeybindsC Project/Assets/UKeybinds Proyecto/Scripts/ManagerKeyBind.cs
+++ UKeybindsC Project/Assets/UKeybinds Proyecto/Scripts/ManagerKeyBind.cs
@@ -32,7 +32,11 @@ namespace MoonAntonio
 		/// <summary>
 		/// <para>Coleccion de teclas</para>
 		/// </summary>
-		private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();		// Coleccion de teclas
+		private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();       // Coleccion de teclas
+		/// <summary>
+		/// <para>Key actual guardada</para>
+		/// </summary>
+		private GameObject actualKey;														// Key actual guardada
 		#endregion
 
 		#region Inicializadores
@@ -60,6 +64,92 @@ namespace MoonAntonio
 			defender.text = keys["defender"].ToString();
 			saltar.text = keys["saltar"].ToString();
 			correr.text = keys["correr"].ToString();
+		}
+		#endregion
+
+		#region Actualizadores
+		/// <summary>
+		/// <para>Actualizador de <see cref="ManagerKeyBind"/>.</para>
+		/// </summary>
+		private void Update()// Actualizador de ManagerKeyBind
+		{
+			// Seguimiento de los inputs
+			if (Input.GetKeyDown(keys["arriba"]))
+			{
+				Debug.Log("Arriba");
+			}
+
+			if (Input.GetKeyDown(keys["abajo"]))
+			{
+				Debug.Log("Abajo");
+			}
+
+			if (Input.GetKeyDown(keys["derecha"]))
+			{
+				Debug.Log("Derecha");
+			}
+
+			if (Input.GetKeyDown(keys["izquierda"]))
+			{
+				Debug.Log("Izquierda");
+			}
+
+			if (Input.GetKeyDown(keys["atacar"]))
+			{
+				Debug.Log("Atacar");
+			}
+
+			if (Input.GetKeyDown(keys["defender"]))
+			{
+				Debug.Log("Defender");
+			}
+
+			if (Input.GetKeyDown(keys["saltar"]))
+			{
+				Debug.Log("Saltar");
+			}
+
+			if (Input.GetKeyDown(keys["correr"]))
+			{
+				Debug.Log("Correr");
+			}
+
+
+		}
+		#endregion
+
+		#region GUI
+		/// <summary>
+		/// <para>GUI</para>
+		/// </summary>
+		private void OnGUI()// GUI
+		{
+			// Comprobamos si la key actual no es nula
+			if (actualKey != null)
+			{
+				// Agregamos un evento
+				Event e = Event.current;
+				if (e.isKey)
+				{
+					// Asignamos la key obtenida del evento a nuestra base de keys
+					keys[actualKey.name] = e.keyCode;
+
+					// Actualizamos la UI de la key
+					actualKey.transform.GetChild(0).GetComponent<Text>().text = e.keyCode.ToString();
+					actualKey = null;
+				}
+			}
+		}
+		#endregion
+
+		#region Metodos
+		/// <summary>
+		/// <para>Cambia la key</para>
+		/// </summary>
+		/// <param name="obj">Objeto</param>
+		public void CambiarKey(GameObject obj)// Cambia la key
+		{
+			actualKey = obj;
 		}
 		#endregion
 	}
